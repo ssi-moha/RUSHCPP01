76c03401d522cfe243dfc825f9877247967fd117
Sinane <ssi-moha@student.42.fr>
2019-06-01 16:25:38 +0200

    Date ok

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include "Modules/Hostname.hpp"
 #include "Modules/Username.hpp"
 #include "Modules/OsInfo.hpp"
+#include "Modules/Date.hpp"
 #include <iostream>
 
 int main(void) {
     Hostname hostname;
     Username username;
     OsInfo osInfo;
+    Date date;
 
     std::cout << hostname.getHostname() << std::endl;
     std::cout << username.getUsername() << std::endl;
     std::cout << osInfo.getVersion() << std::endl;
+    std::cout << date.today() << std::endl;
+    std::cout << date.now() << std::endl;
     return 0;
 }
