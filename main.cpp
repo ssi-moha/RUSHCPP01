754db753d13a247eb486a7f7f77f9dcf1ea0e822
Sinane <ssi-moha@student.42.fr>
2019-06-02 18:45:17 +0200

    Fin

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <fstream>
+#include <map>
+
 #include "Modules/Hostname.hpp"
 #include "Modules/Username.hpp"
 #include "Modules/OsInfo.hpp"
@@ -10,20 +13,42 @@
 #include "IMonitorDisplay.hpp"
 #include "Modules/AMonitorModule.hpp"
 
-int main()
-{	
-    std::vector<AMonitorModule*> moduleTab;
+std::vector<AMonitorModule*>* readConfigFile(char *filename) {
+    std::string line;
+    std::vector<AMonitorModule*> *moduleTab = new std::vector<AMonitorModule*>();
+    std::ifstream configFile (filename);
+    
+    if (configFile.is_open()) {
 
-    moduleTab.push_back(new Hostname());
-    moduleTab.push_back(new Username());
-    moduleTab.push_back(new OsInfo());
-    moduleTab.push_back(new Date());
+        while (getline(configFile, line)) {
+            std::cout << line << std::endl;    
+            if (!line.compare("hostname")) {
+                moduleTab->push_back(new Hostname());
+            } else if (!line.compare("username")) {
+                moduleTab->push_back(new Username());
+            } else if (!line.compare("osinfo")) {
+                moduleTab->push_back(new OsInfo());
+            } else if (!line.compare("date")) {
+                moduleTab->push_back(new Date());
+            }
+        }
+        configFile.close();
+    }
+    else std::cout << "Unable to open file" << std::endl; 
 
+    return moduleTab;
+}
 
-
-    IMonitorDisplay display(moduleTab);
+int main(int ac, char **av)
+{	    
+    if (ac < 2) {
+        std::cout << "No config file." << std::endl;
+    }
+    std::vector<AMonitorModule*> *moduleTab = readConfigFile(av[1]);
+    IMonitorDisplay display(*moduleTab);
     
     display.run();
-    
+
+    delete moduleTab;
     return 0;
 }
